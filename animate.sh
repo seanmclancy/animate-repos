e38b6f99dd679c5f1c366f0e8e93425dfa110896
Sean Clancy <seanmclancy@gmail.com>
2022-03-17 11:04:17 -0700

    clean up the combined log

--- animate.sh
+++ animate.sh
@@ -22,3 +22,5 @@ done
 gource combined.txt --key --title "${title}" --disable-progress --bloom-multiplier 0.7 --seconds-per-day 2 --auto-skip-seconds 1 \
     --bloom-multiplier 0.5 --bloom-intensity 1.4 --hide filenames --seconds-per-day 2 --auto-skip-seconds 1 -1280x720 -o - \
     | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset veryslow -crf 18 -threads 0 -bf 0 "${outfile}.mp4"
+
+rm combined.txt
